8d0a29487df42077f19e012bc0368d4efad5bed5
mukabyl <mukabyl@yamdex.ru>
2023-02-08 20:27:25 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        Oblastar oblasta = Oblastar.valueOf(name.toUpperCase());
+
+        switch (oblasta){
+            case CHUI -> System.out.println(Oblastar.CHUI);
+            case NARYN -> System.out.println(Oblastar.NARYN);
+            case TALAS -> System.out.println(Oblastar.TALAS);
+            case OSH -> System.out.println(Oblastar.OSH);
+            case YSSYK_KOL -> System.out.println(Oblastar.YSSYK_KOL);
+            case JALAL_ABAD -> System.out.println(Oblastar.JALAL_ABAD);
+            case BATKEN -> System.out.println(Oblastar.BATKEN);
+            default -> System.out.println("myndai oblast jok");
+
+        }
     }
 }
--- src/Oblastar.java
+++ src/Oblastar.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public enum Oblastar {
+public enum Oblastar {
+    CHUI("bul oblasta borborubus jaigashkan"),
+    NARYN("Kyshynda abdan suuk"),
+    TALAS("Buurchaktyn mekeni"),
+    OSH("Sulaiman-Toosu menen ozgocho"),
+    BATKEN("Oruktoru abdan daamdu"),
+    YSSYK_KOL("Kyshynda tonbogon kolu bar"),
+    JALAL_ABAD("Koon arbuz erte byshat");
+    private String ozgocholugu;
+
+    Oblastar(String ozgocholugu) {
+        this.ozgocholugu = ozgocholugu;
+    }
+
+    @Override
+    public String toString() {
+        return "Oblastar{" +
+                "ozgocholugu='" + ozgocholugu + '\'' +
+                '}';
+    }
 }
